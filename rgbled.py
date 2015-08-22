ae9932b33524779b2fc779968fcf79d95730b101
ericoporto <ericoporto2008@gmail.com>
2015-08-22 20:24:59 -0300

    updated the usage text.

--- rgbled.py
+++ rgbled.py
@@ -144,5 +144,5 @@ if __name__ == "__main__":
       sys.exit(2)
       sys.exit(0)
   else:
-    print "usage: %s start|stop|restart" % sys.argv[0]
+    print "usage: %s start|stop|restart|color [color]|blink [on|off]" % sys.argv[0]
     sys.exit(2)
